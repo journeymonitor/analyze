f9627ca0de75a4757abfa6153f09d0a849dffd76
Manuel Kiessling <manuel@kiessling.net>
2015-12-30 22:45:24 +0100

    Migrated ApplicationSpec to ScalaTest, temp. disabled IntegrationSpec

--- api/build.sbt
+++ api/build.sbt
@@ -10,7 +10,7 @@ libraryDependencies ++= Seq(
   jdbc,
   cache,
   ws,
-  specs2 % Test
+  "org.scalatestplus" %% "play" % "1.4.0-M3" % "test"
 )
 
 resolvers += "scalaz-bintray" at "http://dl.bintray.com/scalaz/releases"
--- api/test/IntegrationSpec.scala
+++ api/test/IntegrationSpec.scala
@@ -1,5 +1,3 @@
-import org.specs2.mutable._
-import org.specs2.runner._
 import org.junit.runner._
 
 import play.api.test._
@@ -9,6 +7,7 @@ import play.api.test.Helpers._
  * add your integration spec here.
  * An integration test will fire up a whole play application in a real (or headless) browser
  */
+/*
 @RunWith(classOf[JUnitRunner])
 class IntegrationSpec extends Specification {
 
@@ -22,3 +21,4 @@ class IntegrationSpec extends Specification {
     }
   }
 }
+*/
