e435f96cd0a3ab1dcb14c1daf2f0e7e0e0b74c67
Manuel Kiessling <manuel@kiessling.net>
2015-12-30 23:05:29 +0100

    Siwtched to mustBe because that's faster

--- api/test/ApplicationSpec.scala
+++ api/test/ApplicationSpec.scala
@@ -9,14 +9,14 @@ class ApplicationSpec extends PlaySpec with OneAppPerSuite {
     "send 404 on a bad request" in {
       val Some(wrongRoute) = route(FakeRequest(GET, "/boum"))
 
-      status(wrongRoute) mustEqual NOT_FOUND
+      status(wrongRoute) mustBe NOT_FOUND
     }
 
     "render the index page" in {
       val Some(home) = route(FakeRequest(GET, "/"))
 
-      status(home) mustEqual OK
-      contentType(home) mustEqual Some("text/html")
+      status(home) mustBe OK
+      contentType(home) mustBe Some("text/html")
       contentAsString(home) must include ("Your new application is ready.")
     }
   }
