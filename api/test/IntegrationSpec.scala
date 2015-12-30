763ff0f5ba6bf3d770bcebd3dc502c55f39701b2
Manuel Kiessling <manuel@kiessling.net>
2015-12-30 23:06:03 +0100

    Migrated IntegrationSpec to ScalaTest

--- api/test/IntegrationSpec.scala
+++ api/test/IntegrationSpec.scala
@@ -1,24 +1,14 @@
-import org.junit.runner._
+import org.scalatestplus.play._
 
-import play.api.test._
-import play.api.test.Helpers._
-
-/**
- * add your integration spec here.
- * An integration test will fire up a whole play application in a real (or headless) browser
- */
-/*
-@RunWith(classOf[JUnitRunner])
-class IntegrationSpec extends Specification {
+class IntegrationSpec extends PlaySpec with OneBrowserPerSuite with OneServerPerSuite with HtmlUnitFactory {
 
   "Application" should {
 
-    "work from within a browser" in new WithBrowser {
+    "work from within a browser" in {
 
-      browser.goTo("http://localhost:" + port)
+      go to "http://localhost:" + port
 
-      browser.pageSource must contain("Your new application is ready.")
+      pageSource must include ("Your new application is ready.")
     }
   }
 }
-*/
