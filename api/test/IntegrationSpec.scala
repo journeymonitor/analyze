e3934a12f813bff896a16917c8af614297092b11
Manuel Kiessling <manuel@kiessling.net>
2016-01-20 08:21:40 +0100

    More precise integration spec verifies we get the latest statistics

--- api/test/IntegrationSpec.scala
+++ api/test/IntegrationSpec.scala
@@ -13,15 +13,34 @@ class IntegrationSpec extends PlaySpec with OneBrowserPerSuite with OneServerPer
     val uri = CassandraConnectionUri(uriString)
     val session = CassandraClient.createSessionAndInitKeyspace(uri)
 
-    val query =
+    session.execute("TRUNCATE statistics;")
+
+    session.execute(
       """
         |INSERT INTO statistics
         | (testcase_id, testresult_id, datetime_run,               runtime_milliseconds, number_of_200, number_of_400, number_of_500)
         | VALUES
-        | ('testcase1', 'testresult1', '2016-01-07 07:32:12+0000', 987,                  123,           456,           789);
+        | ('testcase1', 'testresult1', '2016-01-07 01:32:12+0000', 111,                  123,           456,           789);
       """.stripMargin
+    )
 
-    session.execute(query)
+    session.execute(
+      """
+        |INSERT INTO statistics
+        | (testcase_id, testresult_id, datetime_run,               runtime_milliseconds, number_of_200, number_of_400, number_of_500)
+        | VALUES
+        | ('testcase1', 'testresult1', '2016-01-07 03:32:12+0000', 333,                  123,           456,           789);
+      """.stripMargin
+    )
+
+    session.execute(
+      """
+        |INSERT INTO statistics
+        | (testcase_id, testresult_id, datetime_run,               runtime_milliseconds, number_of_200, number_of_400, number_of_500)
+        | VALUES
+        | ('testcase1', 'testresult1', '2016-01-07 02:32:12+0000', 222,                  123,           456,           789);
+      """.stripMargin
+    )
   }
 
   override implicit lazy val app: api.Application =
@@ -39,12 +58,12 @@ class IntegrationSpec extends PlaySpec with OneBrowserPerSuite with OneServerPer
       pageSource must include ("Your new application is ready.")
     }
 
-    "return a JSON object with statistics for a given testcase id" in {
+    "return a JSON object with the latest statistics for a given testcase id" in {
       go to "http://localhost:" + port + "/testresults/testcase1/statistics/"
       pageSource mustBe
         """
           |{"testresultId":"testresult1",
-          |"runtimeMilliseconds":987,
+          |"runtimeMilliseconds":333,
           |"numberOf200":123,
           |"numberOf400":456,
           |"numberOf500":789}
